90bcdcf56f09797bad7167eba0555e927b61ca92
Nicolai Pavliuc <pavliuc75@gmail.com>
2021-04-11 15:21:24 +0200

    create

---
+++ react_grocery_todo/src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SwitchList from "./SwitchList";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SwitchList></SwitchList>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ react_grocery_todo/src/components/SwitchList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SwitchList extends React.Component {
+  render() {
+      return (
+        <div>
+            SwitchList
+        </div>
+      );
+  }
+}
+
+export default SwitchList;
